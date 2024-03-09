866b47af8656ddbe48fb0299e570a9fc4d8fb8fc
Shivam Chaugule <114950843+ShivamC93@users.noreply.github.com>
2024-03-09 19:46:52 +0530

    Add files via upload

---
+++ queue4.java
@@ -0,0 +1,65 @@
+public class queue4 {//linkedlist implementation
+    public static class Node{
+        int val;
+        Node next;
+    }
+
+    public static class queueLL{
+        Node head = null;
+        Node tail = null;
+        int size = 0;
+        public void add(int x){
+            Node temp = new Node();
+            if(size==0){
+                head = tail = temp;
+            }
+            else{
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+        public int peek(){
+            if(size==0){
+                System.out.println("queen is empty");
+                return -1;
+            }
+            return head.val;
+        }
+        public int remove(){
+            if(size==0){
+                System.out.println("queen is empty");
+                return -1;
+            }
+            int x = head.val;
+            head =head.next;
+            size--;
+            return x;
+        }
+
+        public void display(){
+            if(size==0){
+                System.out.println("Queue is empty");
+            }
+            Node temp = head;
+            while(temp!=null){
+                System.out.print(temp.val+"");
+                temp=temp.next;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        queueLL q1 =new queueLL();
+        q1.add(1);
+        q1.add(2);
+        q1.add(3);
+        q1.display();
+        System.out.println(q1.peek());
+        q1.remove();
+        q1.display();
+
+
+    }
+}
