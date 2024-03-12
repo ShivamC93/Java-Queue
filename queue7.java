cbdc41712e44381f023bc6014488e45b90c3bcf3
Shivam Chaugule <114950843+ShivamC93@users.noreply.github.com>
2024-03-12 22:21:03 +0530

    Add files via upload

---
+++ queue7.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class queue7{
+    public static void main(String[] args){
+        Queue<Integer> q =new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        System.out.print(q);
+        Stack<Integer> st = new Stack<>();
+        int n = q.size();
+        for(int i=1;i<=(n/2);i++){
+            st.push(q.remove());
+        }//5 6 7 8
+        while(st.size()>0){
+            q.add(st.pop());
+        } // 4 3 2 1
+        for(int i=1;i<=(q.size()/2);i++){
+            st.push(q.remove());
+        }
+        while(st.size()>0){
+            q.add(st.pop());
+            q.add(q.remove());
+        }
+
+        System.out.println(q);
+
+        while(q.size()>0){
+            st.push(q.remove());
+        }
+        while(st.size()>0){
+            q.add(st.pop());
+        }
+        System.out.print(q);
+    }    
+}
